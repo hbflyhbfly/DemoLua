6bf30854da98677fcde565d5534612217608d9cc
syuuhi <hbflyhbfly@gmail.com>
2014-04-02 19:26:14 +0800

    runer

--- Resources/Runer.lua
+++ Resources/Runer.lua
@@ -1,9 +1,21 @@
 
-local function createRunerLayer()
+
+function createRunerLayer()
 	local _runerLayer = CCLayer:create()
-	local _cache = CCSpriteFrameCache:sharedSpriteFrameCache()
-	local _runer = CCSprite:spriteWithSpriteFrame(_cache:spriteFrameByName("loading_01.png"))
+	cache = CCSpriteFrameCache:sharedSpriteFrameCache()
+	local _runer = CCSprite:createWithSpriteFrame(cache:spriteFrameByName("loading_01.png"))
+	local actionFrames = CCArray:create()
+
+	for i=1,10 do
+		actionFrames:addObject(cache:spriteFrameByName("loading_01.png"))
+	end
+
+	local animation = CCAnimation:createWithSpriteFrames(actionFrames,0.3)
+	local  animate = CCAnimate:create(animation)
+	_runerLayer:runAction(CCRepeatForever:create(animate))
 	_runer:setPosition(ccp(200,200))
 	_runerLayer:addChild(_runer)	
 	return _runerLayer
 end
+
+
--- Resources/hello.lua
+++ Resources/hello.lua
@@ -1,4 +1,4 @@
-require "AudioEngine" 
+require "AudioEngine"
 require "Runer"
 -- for CCLuaEngine traceback
 function __G__TRACKBACK__(msg)
@@ -23,80 +23,12 @@ local function main()
     --print(visibleSize)
     local origin = CCDirector:sharedDirector():getVisibleOrigin()
 
-    -- add the moving dog
-    local function creatDog()
-        local frameWidth = 105
-        local frameHeight = 95
 
-        -- create dog animate
-        local textureDog = CCTextureCache:sharedTextureCache():addImage("Res.png")
-        local rect = CCRectMake(0, 0, frameWidth, frameHeight)
-        local frame0 = CCSpriteFrame:createWithTexture(textureDog, rect)
-        rect = CCRectMake(frameWidth, 0, frameWidth, frameHeight)
-        local frame1 = CCSpriteFrame:createWithTexture(textureDog, rect)
-
-        local spriteDog = CCSprite:createWithSpriteFrame(frame0)
-        spriteDog.isPaused = false
-        spriteDog:setPosition(origin.x, origin.y + visibleSize.height / 4 * 3)
-
-        local animFrames = CCArray:create()
-
-        animFrames:addObject(frame0)
-        animFrames:addObject(frame1)
-
-        local animation = CCAnimation:createWithSpriteFrames(animFrames, 0.5)
-        local animate = CCAnimate:create(animation);
-        spriteDog:runAction(CCRepeatForever:create(animate))
-
-        -- moving dog at every frame
-        local function tick()
-            if spriteDog.isPaused then return end
-            local x, y = spriteDog:getPosition()
-            if x > origin.x + visibleSize.width then
-                x = origin.x
-            else
-                x = x + 1
-            end
-
-            spriteDog:setPositionX(x)
-        end
-
-        CCDirector:sharedDirector():getScheduler():scheduleScriptFunc(tick, 0, false)
-
-        return spriteDog
-    end
 
     -- create farm
-    local function createLayerFarm()
-        local layerFarm = CCLayer:create()
+    local function createHelloLayer()
+        local layerHello = CCLayer:create()
 
-        -- add in farm background
-        local bg = CCSprite:create("farm.jpg")
-        bg:setPosition(origin.x + visibleSize.width / 2 + 80, origin.y + visibleSize.height / 2)
-        layerFarm:addChild(bg)
-
-        -- add land sprite
-        for i = 0, 3 do
-            for j = 0, 1 do
-                local spriteLand = CCSprite:create("land.png")
-                spriteLand:setPosition(200 + j * 180 - i % 2 * 90, 10 + i * 95 / 2)
-                layerFarm:addChild(spriteLand)
-            end
-        end
-
-        -- add crop
-        local frameCrop = CCSpriteFrame:create("crop.png", CCRectMake(0, 0, 105, 95))
-        for i = 0, 3 do
-            for j = 0, 1 do
-                local spriteCrop = CCSprite:createWithSpriteFrame(frameCrop);
-                spriteCrop:setPosition(10 + 200 + j * 180 - i % 2 * 90, 30 + 10 + i * 95 / 2)
-                layerFarm:addChild(spriteCrop)
-            end
-        end
-
-        -- add moving dog
-        local spriteDog = creatDog()
-        layerFarm:addChild(spriteDog)
 
         -- handing touch events
         local touchBeginPoint = nil
@@ -104,7 +36,6 @@ local function main()
         local function onTouchBegan(x, y)
             cclog("onTouchBegan: %0.2f, %0.2f", x, y)
             touchBeginPoint = {x = x, y = y}
-            spriteDog.isPaused = true
             -- CCTOUCHBEGAN event must return true
             return true
         end
@@ -122,7 +53,6 @@ local function main()
         local function onTouchEnded(x, y)
             cclog("onTouchEnded: %0.2f, %0.2f", x, y)
             touchBeginPoint = nil
-            spriteDog.isPaused = false
         end
 
         local function onTouch(eventType, x, y)
@@ -135,52 +65,10 @@ local function main()
             end
         end
 
-        layerFarm:registerScriptTouchHandler(onTouch)
-        layerFarm:setTouchEnabled(true)
+        layerHello:registerScriptTouchHandler(onTouch)
+        layerHello:setTouchEnabled(true)
 
-        return layerFarm
-    end
-
-
-    -- create menu
-    local function createLayerMenu()
-        local layerMenu = CCLayer:create()
-
-        local menuPopup, menuTools, effectID
-
-        local function menuCallbackClosePopup()
-            -- stop test sound effect
-            AudioEngine.stopEffect(effectID)
-            menuPopup:setVisible(false)
-        end
-
-        local function menuCallbackOpenPopup()
-            -- loop test sound effect
-            local effectPath = CCFileUtils:sharedFileUtils():fullPathForFilename("effect1.wav")
-            effectID = AudioEngine.playEffect(effectPath)
-            menuPopup:setVisible(true)
-        end
-
-        -- add a popup menu
-        local menuPopupItem = CCMenuItemImage:create("menu2.png", "menu2.png")
-        menuPopupItem:setPosition(0, 0)
-        menuPopupItem:registerScriptTapHandler(menuCallbackClosePopup)
-        menuPopup = CCMenu:createWithItem(menuPopupItem)
-        menuPopup:setPosition(origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 2)
-        menuPopup:setVisible(false)
-        layerMenu:addChild(menuPopup)
-
-        -- add the left-bottom "tools" menu to invoke menuPopup
-        local menuToolsItem = CCMenuItemImage:create("menu1.png", "menu1.png")
-        menuToolsItem:setPosition(0, 0)
-        menuToolsItem:registerScriptTapHandler(menuCallbackOpenPopup)
-        menuTools = CCMenu:createWithItem(menuToolsItem)
-        local itemWidth = menuToolsItem:getContentSize().width
-        local itemHeight = menuToolsItem:getContentSize().height
-        menuTools:setPosition(origin.x + itemWidth/2, origin.y + itemHeight/2)
-        layerMenu:addChild(menuTools)
-
-        return layerMenu
+        return layerHello
     end
 
     -- play background music, preload effect
@@ -194,8 +82,7 @@ local function main()
 
     -- run
     local sceneGame = CCScene:create()
-    sceneGame:addChild(createLayerFarm())
-    sceneGame:addChild(createLayerMenu())
+    sceneGame:addChild(createHelloLayer())
     sceneGame:addChild(createRunerLayer())
     CCDirector:sharedDirector():runWithScene(sceneGame)
 end
